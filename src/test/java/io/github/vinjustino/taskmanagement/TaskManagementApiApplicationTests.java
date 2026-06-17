f276d0a71a5e47d670dfa8297b06d433a5203a65
Vinicius Justino <vinjustino.dev@gmail.com>
2026-06-17 12:09:23 -0300

    chore: initial project setup

---
+++ src/test/java/io/github/vinjustino/taskmanagement/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vinjustino.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
